2efe6ad4f33d7373216c4b8da4824cb607deeee2
William Song <30965609+Freakwill@users.noreply.github.com>
2018-04-25 18:16:33 +0800

    add files

---
+++ README.md
@@ -0,0 +1,50 @@
+Introduction
+=============
+
+Abstract
+----------
+extension of pyparsing.
+
+Keywords
+----------
+Text Process
+
+Feature:
+
+    1. mixedExpression
+
+
+Content
+=========
+
+Classes::
+
+    Wordx: powerful Word
+    CharsNot: powerful CharsNotIn
+    LeadedBy: as FollowedBy
+    MeanWhile:
+    LinenStart:
+
+    BaseAction: Base Class of Actions
+
+Functions::
+
+    keyRange(s)
+    ordRange(s)
+    chrRange(s)
+    CJK: for matching Chinese Japanese Korean
+    enumeratedItems
+    delimitedMatrix: delimitedList with two seps
+
+
+Example
+=========
+
+    w = Wordx(lambda x: x in {'a', 'b', 'c', 'd'})
+    M = delimitedMatrix(w, ch1=' ', ch2=pp.Regex('\n+').leaveWhitespace())
+    p = M.parseString('a b\nc d')
+    print(p.asList())
+
+    s = '''[1]hehe
+    [2]hehe'''
+    print(enumeratedItems().parseString(s))
---
+++ setup.py
@@ -0,0 +1,60 @@
+import codecs
+import os
+# import sys
+ 
+#try:
+from setuptools import setup
+#except:
+#    from distutils.core import setup
+
+ 
+def read(fname):
+    """
+    define read method to read the long description.
+    It will be shown in PyPI"""
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+ 
+ 
+ 
+NAME = "pyparsing_ext"
+
+DESCRIPTION = "A cool tool for parsing. It is an extension of pyparsing"
+ 
+LONG_DESCRIPTION = read("README.md")
+ 
+KEYWORDS = "Parsing, Text Processing"
+ 
+AUTHOR = "William Song"
+ 
+AUTHOR_EMAIL = "songcwzjut@163.com"
+ 
+URL = "https://github.com/Freakwill/pyparsing_ext"
+ 
+VERSION = "0.0.2" # update the version before uploading
+ 
+LICENSE = "MIT"
+
+ 
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: Public Domain',  # Public Domain
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Natural Language :: English'
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    # packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+)
